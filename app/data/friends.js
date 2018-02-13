a7e8ba8d38113b73ed45dee24cc6cff11ad7eb04
Akhila S Naik <Akhilasnaik@gmail.com>
2018-02-12 21:32:19 -0500

    Friends Friend Finder

---
+++ app/data/friends.js
@@ -0,0 +1,115 @@
+var friends = [
+  {
+    name: "Rachel Greene",
+    photo: "https://i.pinimg.com/736x/57/60/3c/57603c84715b8011d6a7e8609f4c748c--rachel-green-style-rachel-green-friends.jpg",
+    scores: [
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2",
+      "2"
+    ]
+  },
+  {
+    name: "Phoebe Buffay",
+    photo: "https://i.ytimg.com/vi/Gi43eJUSyGE/maxresdefault.jpg",
+    scores: [
+      "5",
+      "5",
+      "1",
+      "5",
+      "1",
+      "3",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Monica Gellar",
+    photo: "https://qph.ec.quoracdn.net/main-qimg-6dd0482108ca31a44bc909ed7104aa04",
+    scores: [
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3",
+      "3"
+    ]
+  },
+  {
+    name: "Joey Tribiani",
+    photo: "http://s3.r29static.com/bin/entry/2a8/x/1473035/image.gif",
+    scores: [
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Ross Gellar",
+    photo: "https://pmctvline2.files.wordpress.com/2015/01/friends-skippable-ross-tan.jpg?w=514",
+    scores: [
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Chandler Bing",
+    photo: "https://78.media.tumblr.com/789e720131f8938875fc92f849148386/tumblr_nwha7srEmC1uhg3mjo6_250.gif",
+    scores: [
+      "1",
+      "5",
+      "5",
+      "1",
+      "5",
+      "5",
+      "5",
+      "5",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Janice Litman",
+    photo: "https://78.media.tumblr.com/3aa6283bfbffb0f929d9e59ab9e2bc11/tumblr_ntvmgnE0Wj1u41q2oo1_250.gif",
+    scores: [
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4",
+      "4"
+    ]
+  }
+  ];
+module.exports = friends;
